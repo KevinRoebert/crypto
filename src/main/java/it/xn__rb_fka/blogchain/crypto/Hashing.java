27d8cec4799561fd40a23caa73dc5b219dd04fd2
Kevin Röbert <dev@xn--rb-fka.it>
2018-12-13 23:01:42 +0100

    Adding byte-array hashing

--- src/main/java/it/xn__rb_fka/blogchain/crypto/Hashing.java
+++ src/main/java/it/xn__rb_fka/blogchain/crypto/Hashing.java
@@ -13,11 +13,22 @@ public class Hashing {
 	 * @return SHA256 Hash von message
 	 */
 	public static String sha256(String message) {
+		return sha256(message.getBytes(StandardCharsets.UTF_8));
+	}
+
+	/**
+	 * Berechnet einen SHA256 Hashwert von einem gegebenen Byte-Array.
+	 * 
+	 * @param message
+	 *            Byte-Array von dem der Hashwert berechnet werden soll
+	 * @return SHA256 Hash von message
+	 */
+	public static String sha256(byte[] message) {
 		StringBuffer sb = new StringBuffer();
 
 		try {
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			md.update(message.getBytes());
+			md.update(message);
 			byte byteData[] = md.digest();
 
 			for (int i = 0; i < byteData.length; i++) {
@@ -38,6 +49,17 @@ public class Hashing {
 	 * @return double SHA256 Hash von message
 	 */
 	public static String doubleHash(String message) {
+		return doubleHash(message.getBytes(StandardCharsets.UTF_8));
+	}
+	
+	/**
+	 * Erstellt einen double SHA256 Hashwert von einem gegebenen Byte-Array.
+	 * 
+	 * @param message
+	 *            Byte-Array von dem der Hashwert berechnert werden soll
+	 * @return double SHA256 Hash von message
+	 */
+	public static String doubleHash(byte[] message) {
 		return sha256(sha256(message));
 	}
 
@@ -49,11 +71,23 @@ public class Hashing {
 	 * @return MD5 Hash von message
 	 */
 	public static String md5(String message) {
+		return md5(message.getBytes(StandardCharsets.UTF_8));
+	}
+	
+	/**
+	 * Erstellt einen MD5 Hashwert von einem gegebenen Byte-Array.
+	 * 
+	 * @param message
+	 *            Byte-Array von dem der Hashwert berechnet werden soll
+	 * @return MD5 Hash von message
+	 */
+	public static String md5(byte[] message) 
+	{
 		StringBuffer sb = new StringBuffer();
 
 		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
-			byte byteData[] = md.digest(message.getBytes(StandardCharsets.UTF_8));
+			byte byteData[] = md.digest(message);
 
 			for (byte b : byteData) {
 				sb.append(String.format("%02x", b));
